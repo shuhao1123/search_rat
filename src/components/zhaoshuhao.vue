799c2617d0d334bc749930d96f50a23ab066b31e
shuhao1123 <1723433671@qq.com>
2018-07-12 16:40:49 +0800

    banner

--- src/components/zhaoshuhao.vue
+++ src/components/zhaoshuhao.vue
@@ -1,19 +1,15 @@
 <template>
-    <div style="width:90%;margin:20px auto">
-        <div class="container-fluid">
-            <div class="row table table-bordered">
-                <div class="col-md-2 col-sm-4" v-for="(item,index) in data" :key="index">{{item}}</div>
-                <!-- <div class="col-md-2 col-sm-4">.col-md-2</div>
-                <div class="col-md-2 col-sm-4">.col-md-3</div>
-                <div class="col-md-2 col-sm-4">.col-md-4</div>
-                <div class="col-md-2 col-sm-4">.col-md-5</div>
-                <div class="col-md-2 col-sm-4">.col-md-6</div>
-                <div class="col-md-2 col-sm-4">.col-md-7</div>
-                <div class="col-md-2 col-sm-4">.col-md-8</div>
-                <div class="col-md-2 col-sm-4">.col-md-9</div>
-                <div class="col-md-2 col-sm-4">.col-md-10</div>
-                <div class="col-md-2 col-sm-4">.col-md-11</div>
-                <div class="col-md-2 col-sm-4">.col-md-12</div> -->
+    <div>
+        <div><img src="./../assets/ejd9qJo8XP.jpg" style="width:100%"></div>
+
+        <div style="width:95%;margin:20px auto">
+            <div class="container-fluid">
+                <div class="row table table-bordered">
+                    <div class="col-md-3 col-sm-4" v-for="(item,index) in data" :key="index">
+                        <img :src=item[1] style="width:100%">
+                        <p style="height:50px;line-height:25px;">{{item[0]}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -23,34 +19,32 @@ export default {
     data() {
         return {
             data: [
-                "店铺预警",
-                "流量把控",
-                "选品定价",
-                "客群定位",
-                "搜索绩效",
-                "搜索鼠",
-                "店铺预警",
-                "流量把控",
-                "选品定价",
-                "客群定位",
-                "搜索绩效",
-                "搜索鼠",
-                "店铺预警",
-                "流量把控",
-                "选品定价",
-                "客群定位",
-                "搜索绩效",
-                "搜索鼠"
+                [
+                    "Java并发编程与高并发解决方案",
+                    "https://img1.mukewang.com/szimg/5aaa55850001a3ef10800600.jpg"
+                ],
+                [
+                    "玩转算法面试 从真题到思维全面提升算法思维",
+                    "https://img2.mukewang.com/szimg/5a1f65a900015d1505400300.jpg"
+                ],
+                [
+                    "Spring Cloud微服务实战",
+                    "https://img3.mukewang.com/5b20d7dd000124da06000338-240-135.jpg"
+                ],
+                [
+                    "Java生产环境下性能监控与调优详解",
+                    "https://img2.mukewang.com/szimg/5b384772000132c405400300.jpg"
+                ]
             ]
         };
     }
 };
 </script>
 <style scoped>
-.row{
-    padding: 10px
+.row {
+    padding: 10px;
 }
-.row div{
+.row div {
     line-height: 30px;
 }
 </style>
