3d76484f6e3186ed57f0cbda970fec8ab2e1bc0d
shuhao1123 <1723433671@qq.com>
2018-07-20 17:51:54 +0800

    swiper

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -2,9 +2,7 @@
     <div class="swiper">
         <div class="swiper-container">
             <div class="swiper-wrapper">
-                <div class="swiper-slide"><img src="./../assets/40a6232c9b4e3ddd72afe18ea11f9a12.jpg"></div>
-                <div class="swiper-slide"><img src="./../assets/lMQYnywEXD.jpg"></div>
-                <div class="swiper-slide"><img src="./../assets/9dd6736bb90966289613b9ec85a47853.jpg"></div>
+                <div class="swiper-slide" v-for="(item,index) in src1" :key="index"><img :src="item"></div>
             </div>
             <div class="swiper-pagination"></div>
         </div>
@@ -14,17 +12,35 @@
 <script>
 export default {
     data() {
-        return {};
+        return {
+            src1: [
+                require("./../assets/496efd5414ce543a6bf12d4cf83532c6.jpg"),
+                require("./../assets/3892c1eeb3dd03fba4af09b136690a6e.jpg"),
+                require("./../assets/5e2aa29d1e9b9bd1d610b49560921202.jpg")
+            ]
+        };
+    },
+    beforeMount() {},
+    created() {
+        if ($("#app").width() < 750) {
+            this.src1 = [
+                require("./../assets/swiper3.jpg"),
+                require("./../assets/swiper2.jpg"),
+                require("./../assets/swiper1.jpg")
+            ];
+        }
     },
     mounted() {
         var mySwiper = new Swiper(".swiper-container", {
             autoplay: 3000, //可选选项，自动滑动
-            autoplayDisableOnInteraction : false,
+            autoplayDisableOnInteraction: false,
             // initialSlide: 2, //设定初始化时slide的索引。
             grabCursor: true, //设置为true时，鼠标覆盖Swiper时指针会变成手掌形状，拖动时指针会变成抓手形状。
             loop: true,
             pagination: ".swiper-pagination",
-            paginationClickable :true,
+            paginationClickable: true,
+            observer: true, //修改swiper自己或子元素时，自动初始化swiper
+            observeParents: true //修改swiper的父元素时，自动初始化swiper
         });
     }
 };
