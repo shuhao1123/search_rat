6a17282a41d2423ebcfba1f72d53f93f9555d260
shuhao1123 <1723433671@qq.com>
2018-07-20 15:18:57 +0800

    swiper

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -6,18 +6,27 @@
                 <div class="swiper-slide"><img src="./../assets/lMQYnywEXD.jpg"></div>
                 <div class="swiper-slide"><img src="./../assets/9dd6736bb90966289613b9ec85a47853.jpg"></div>
             </div>
+            <div class="swiper-pagination"></div>
         </div>
+
     </div>
 </template>
 <script>
-export default {};
-window.onload = function() {
-    var mySwiper = new Swiper(".swiper-container", {
-        autoplay: 3000, //可选选项，自动滑动
-        initialSlide: 2, //设定初始化时slide的索引。
-        grabCursor: true, //设置为true时，鼠标覆盖Swiper时指针会变成手掌形状，拖动时指针会变成抓手形状。
-        loop : true,
-    });
+export default {
+    data() {
+        return {};
+    },
+    mounted() {
+        var mySwiper = new Swiper(".swiper-container", {
+            autoplay: 3000, //可选选项，自动滑动
+            autoplayDisableOnInteraction : false,
+            // initialSlide: 2, //设定初始化时slide的索引。
+            grabCursor: true, //设置为true时，鼠标覆盖Swiper时指针会变成手掌形状，拖动时指针会变成抓手形状。
+            loop: true,
+            pagination: ".swiper-pagination",
+            paginationClickable :true,
+        });
+    }
 };
 </script>
 <style scoped>
